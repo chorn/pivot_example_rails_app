46a5da194ac557854399252f6a7eb094d61cca58
chorn <chorn@chorn.com>
2015-12-31 09:34:48 -0500

    Chapter 6 - Add tests for the new pivot

--- spec/pivots/a__m__user_pivot_spec.rb
+++ spec/pivots/a__m__user_pivot_spec.rb
@@ -8,6 +8,42 @@ RSpec.describe Pivot do
     it 'exists' do
       expect(subject.respond_to?(:a__m__user__admin__pivot_1?)).to eq true
     end
-  end
 
+    context "with an admin user" do
+      let(:user) { build :pivot_user, is_admin:true }
+
+      describe "Rails.env == development" do
+        it 'is true' do
+          allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+          expect(user.admin?).to eq true
+        end
+      end
+
+      describe "Rails.env == production" do
+        it 'is true' do
+          allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+          expect(user.admin?).to eq true
+        end
+      end
+    end
+
+    context "with a non-admin user " do
+      let(:user) { build :pivot_user, is_admin:false }
+
+      describe "Rails.env == development" do
+        it 'is true' do
+          allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("development"))
+          expect(user.admin?).to eq true
+        end
+      end
+
+      describe "Rails.env == production" do
+        it 'is false' do
+          allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+          expect(user.admin?).to eq false
+        end
+      end
+    end
+
+  end
 end
