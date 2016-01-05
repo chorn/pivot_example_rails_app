73bba534019f2f4f03dee81c09778eaa5d8caf84
chorn <chorn@chorn.com>
2016-01-05 07:55:16 -0500

    Chapter 7 - Add TAGS to the new pivot

--- lib/pivot/a__m__user.rb
+++ lib/pivot/a__m__user.rb
@@ -1,8 +1,9 @@
 module Pivot
   class Master
 
-		# TRUE - Allows or displays admin actions
-		# FALSE - Blocks or hides admin actions
+		# TRUE:  Allows or displays admin actions
+		# FALSE: Blocks or hides admin actions
+		# TAGS:  user, admin, rails_env, development
 
     def a__m__user__admin__pivot_1?(is_admin)
       Rails.env.development? || is_admin
