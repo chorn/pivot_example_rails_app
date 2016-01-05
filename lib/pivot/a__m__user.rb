bf767c2005a4545f57e8b9812660bdea52d50eef
chorn <chorn@chorn.com>
2016-01-05 07:53:08 -0500

    Chapter 7 - Document the true/false paths of the new pivot

--- lib/pivot/a__m__user.rb
+++ lib/pivot/a__m__user.rb
@@ -1,5 +1,9 @@
 module Pivot
   class Master
+
+		# TRUE - Allows or displays admin actions
+		# FALSE - Blocks or hides admin actions
+
     def a__m__user__admin__pivot_1?(is_admin)
       Rails.env.development? || is_admin
     end
